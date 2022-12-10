5a1cbbf5ca6ed3a81d728b3e95cb98c7027e67fe
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2022-12-11 00:32:35 +0500

    fixed the black bar

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -21,7 +21,7 @@ export const Footer = () => {
     return (
         <div className="p-4 md:p-10 bg-gradient-to-b from-white to-lightgray text-black font-avenir">
             <div className="flex flex-col-reverse md:grid grid-cols-3 sm:mx-16 md:mx-32 lg:md-64 gap-24">
-                <div className="ml-10 w-full flex flex-col justify-start">
+                <div className="w-full flex flex-col justify-start">
                     {footerClickables.map((item) => (footerClicks(item.title, item.href)))}
                 </div>
                 <div className="flex flex-col">
