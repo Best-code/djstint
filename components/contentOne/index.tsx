fded6b38f2b2ff0d22917eaf0012987a8e51033b
best-code <cpmraygun@gmail.com>
2022-12-09 19:41:27 -0500

    GoodSpotContentOne

--- components/contentOne/index.tsx
+++ components/contentOne/index.tsx
@@ -7,7 +7,7 @@ export const ContentOne = () => {
             <div className="w-3/4">
                 <div className="w-full sm:h-3/4 h-full flex">
                     <Parallax className="h-full w-full" speed={20}>
-                        <div className="flex flex-row sm:flex-col px-8 py-16 w-2/3">
+                        <div className="flex flex-col px-8 py-16 lg:w-2/3 w-full">
                             <span className="text-5xl py-20 w-1/2 text-white">
                                 Your Car. Cooler.
                             </span>
