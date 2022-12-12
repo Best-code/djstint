f3d38813bb12a6974305f510ed78ed01c029020d
best-code <cpmraygun@gmail.com>
2022-12-11 21:37:35 -0500

    Set gallery picture count

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,20 +1,32 @@
+import { RoundaboutLeft } from "@mui/icons-material"
 import { AnyRecord } from "dns"
 import { NextPage } from "next"
 import Image from "next/image"
 
 const Gallery: NextPage = ({ feed }: any) => {
     const images = feed.data
+
+    const percentageOfFeed = 10
+    const postCount = Object.keys(images).length
+    const restrictedImages = [...Array(Math.round(postCount/percentageOfFeed))] 
+
+    const displayEntireFeed = true
     return (
 
         <div className="w-screen py-12 flex justify-center bg-gradient-to-b from-black via-dark to-light font-avenir">
             <div className=" h-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 sm:gap-8 sm:justify-between">
-                {images.map((image: any) =>
+                {displayEntireFeed && images.map((image: any) =>
                     [
                         <div key={image.id}>
-                            <img src={image.media_url} alt={image.caption} />
-                            
+                            <img src={image.media_url} className="hover:scale-125" alt={image.caption} /> 
                         </div>
                     ])}
+                   {!displayEntireFeed && restrictedImages.map((_,i)=>
+                    [
+                        <div key={images[i].id}>
+                            <img src={images[i].media_url} className="hover:scale-125" alt={images[i].caption} /> 
+                        </div>
+                    ])} 
             </div>
         </div>
     )
