838ff7ada2637c6a57ce1692b6244d0e92bd47fd
best-code <cpmraygun@gmail.com>
2022-12-11 20:51:42 -0500

    Updated Call to action with font size transition on hover

--- components/CallToAction/index.tsx
+++ components/CallToAction/index.tsx
@@ -2,26 +2,24 @@ import { Parallax } from "react-scroll-parallax"
 import { mailto } from "../MailTo"
 
 export default function CallToAction() {
-    return (
-      <div className="bg-inherit h-full">
-        <Parallax className="flex" speed={10}>
-        <div className="mx-auto max-w-7xl py-24 px-4 sm:px-6 lg:items-center lg:justify-between lg:py-16 lg:px-8">
-          <h2 className="text-3xl font-bold tracking-tight text-dark sm:text-4xl">
-            <span className="block pt-12 sm:pt-0">Ready to dive in?</span>
-            <span className="block text-dark">Schedule your tint today.</span>
-          </h2>
-          <div className="mt-4 lg:mt-0 lg:flex-shrink-0">
-            <div className="rounded-md flex w-full ">
-              <a
-                href={mailto}
-                className=" items-center justify-center rounded-md border border-transparent bg-dark px-5 py-3 text-base font-medium text-white hover:bg-black"
-              >
-                Get started
-              </a>
-            </div>
+  return (
+    <Parallax className="w-full flex justify-center" speed={10}>
+      <div className="w-full flex max-w-7xl flex-col py-24 px-4 sm:px-6 lg:justify-between lg:py-16 lg:px-8">
+        <h2 className="text-3xl sm:text-4xl hover:text-5xl transition-all flex flex-col text-center font-bold tracking-tight text-dark">
+          <span className="pt-12 sm:pt-0">Ready to dive in?</span>
+          <span className="text-dark">Schedule your tint today.</span>
+        </h2>
+        <div className="mt-4 lg:flex-shrink-0 w-full">
+          <div className="rounded-md flex w-full justify-center">
+            <a
+              href={mailto}
+              className=" items-center justify-center rounded-md border border-transparent bg-dark px-5 hover:px-7 py-3 hover:py-4 transition-all text-base font-medium text-white hover:bg-black"
+            >
+              Get started
+            </a>
           </div>
         </div>
-        </Parallax>
       </div>
-    )
-  }
+    </Parallax>
+  )
+}
