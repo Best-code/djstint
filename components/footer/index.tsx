0ea325c318ecc6abe66cf6b6ff75b8392ee9f8dc
best-code <cpmraygun@gmail.com>
2022-12-15 01:30:47 -0500

    Web Tech CD

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -24,14 +24,16 @@ export const Footer = () => {
             <div className="grid grid-cols-2 sm:mx-16 md:mx-32 lg:md-64 xs:gap-6 sm:gap-24 items-center ">
                 <div className=" w-full flex flex-col xl:flex-row flex-wrap justify-start">
                     {footerClickables.map((item) => (footerClicks(item.title, item.href)))}
+                    <div className="w-full py-2 flex items-center justify-start pl-2">
+                        Created By Web Tech CD
+                    </div>
                 </div>
                 <div className="flex flex-col">
                     <div className="text-3xl">
                         About Us
                     </div>
                     <div className="text-md py-2">
-                    Specializing in automotive, commercial and residential tinting! Professional quality job for a friend
-
+                        Specializing in automotive, commercial and residential tinting! Professional quality job for a friend
                     </div>
                 </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
+import { SvgIcon } from '@mui/material'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { ContentOne } from '../components/contentOne'
 import { ContentTwo } from '../components/contentTwo'
-
 import { Widgets } from '../components/widgets'
-import { ContentThree } from '../components/contentThree'
-import { Footer } from '../components/footer'
-import Header from '../components/header'
-import { Logo } from '../components/Logo'
-import CallToAction from '../components/CallToAction'
 
 const Home: NextPage = () => {
   return (
