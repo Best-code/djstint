8b2f5bdc89cfaa7a6902ab8d50765c374cfe3019
best-code <cpmraygun@gmail.com>
2022-12-16 15:22:17 -0500

    Logo

--- components/WebTechCD/index.tsx
+++ components/WebTechCD/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const WebTechCD = () => {
     return (
         <a className="text-2xl font-bold font-serif" href="https://www.webtechcd.com">
-            <Image src="/LOGO.svg" height={476} width={151} alt="LOGO" />
+            <Image src="/Logo.svg" height={476} width={151} alt="LOGO" />
         </a>
     )
 
